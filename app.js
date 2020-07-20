9c2ad91d10fb2f396472a62645bbf51354182a71
ike-njoku <31851617+ike-njoku@users.noreply.github.com>
2020-07-20 01:18:01 -0700

    defined methods in Timer class

---
+++ app.js
@@ -0,0 +1,21 @@
+// timer.js
+// create the timer class that contains all timer methods
+class Timer {
+    constructor() {}
+        // reset the counter parameters/display parameters
+    reset() {}
+
+    // start a new countDown/Timer
+    start() {}
+
+    // pause the countdown/timer
+    pause() {}
+
+    // stop/cancel the running timer/countdown
+    stop() {}
+
+    // update the display
+    updateDisplay() {}
+
+
+}

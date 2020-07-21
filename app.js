dbafc196b75fcc31fd7c69e05c892222dc7797d2
ike-njoku <31851617+ike-njoku@users.noreply.github.com>
2020-07-20 21:54:32 -0700

    updated files

--- app.js
+++ app.js
@@ -1,9 +1,34 @@
 // timer.js
+
+
+
+// define constants
+
+// countDownContainer
+const countDownContainer = document.querySelector('[data-countDownContainer]');
+
+// restart timer button
+const restartButton = document.querySelector('[data-restart]');
+
+
+
+
+
+
+
+
+
 // create the timer class that contains all timer methods
 class Timer {
     constructor() {}
         // reset the counter parameters/display parameters
-    reset() {}
+
+    // add new event
+    addNew() {}
+
+    restart() {
+        window.alert('hello world');
+    }
 
     // start a new countDown/Timer
     start() {}
@@ -19,3 +44,12 @@ class Timer {
 
 
 }
+
+const timer = new Timer();
+
+// attach functions
+
+restartButton.addEventListener('click', () => timer.restart());
+
+//hide countdownContainer by default
+countDownContainer.style.display = 'none';

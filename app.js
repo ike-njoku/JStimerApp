fa6d2d6347c64a0c4f26f11f97a362adf02b32bf
ike-njoku <31851617+ike-njoku@users.noreply.github.com>
2020-07-21 23:47:34 -0700

    updated display, dynamically render time setters in js

--- app.js
+++ app.js
@@ -1,21 +1,68 @@
 // timer.js
 
-
-
 // define constants
 
 // countDownContainer
 const countDownContainer = document.querySelector('[data-countDownContainer]');
+// addNew container
+const addNewContainer = document.querySelector('[data-addNew]');
 
+// start Button
+const startButton = document.querySelector('[data-start]');
+// pause Button
+const pauseButton = document.querySelector('[data-pause]');
+// stop Button
+const stopBotton = document.querySelector('[data-stop]');
 // restart timer button
 const restartButton = document.querySelector('[data-restart]');
 
+// input (add timerTitle)
+const titleForm = document.querySelector('[data-titleForm]');
+
+// select hours
+const selectHrs = document.querySelector('[data-selectHrs]');
+
+// select minutes
+const selectMin = document.querySelector('[data-selectMin]');
+
+// generally select timeSetters
+const timesetters = document.querySelectorAll('[data-setTime]');
+
+// select seconds
+const selectSec = document.querySelector('[data-selectSec]');
 
 
 
+// dynamically generate number of seconds and minutes (1-60)
+timesetters.forEach(timeSetter => {
+    // 60 represents seconds and minutes
+    let limit = 60;
+    // set parameters
+
+    for (let i = 0; i < limit; i++) {
+        // increment the value of the next option
+
+        // create an option tag
+        let createOption = document.createElement('option');
+
+        // add content innerHtml to th option created
+        createOption.innerText = i;
+
+        // create a value attribute for each option tag
+        createOption.setAttribute('value', i);
+
+        // append the  option tag to the timesetter
+        timeSetter.appendChild(createOption);
+
+    }
+
+});
 
 
+//hide countdownContainer by default
+// countDownContainer.style.display = 'none';
 
+// dynamically create hours, minutes and secnds
 
 
 // create the timer class that contains all timer methods
@@ -26,9 +73,9 @@ class Timer {
     // add new event
     addNew() {}
 
-    restart() {
-        window.alert('hello world');
-    }
+    // restart countdown
+    restart() {}
+
 
     // start a new countDown/Timer
     start() {}
@@ -49,7 +96,12 @@ const timer = new Timer();
 
 // attach functions
 
-restartButton.addEventListener('click', () => timer.restart());
-
-//hide countdownContainer by default
-countDownContainer.style.display = 'none';
+// startButton
+startButton.addEventListener('click', function validateInput(title) {
+    title = titleForm.value;
+    hours = selectHrs.value;
+    minutes = selectMin.value;
+    seconds = selectSec.value;
+    if (title) timer.addNew(title, hours, minutes, seconds);
+    else window.alert('please add a title to your reminder');
+})
